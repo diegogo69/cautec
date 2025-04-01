61247d47056ef0d4e11233b7b32c14230763a1f2
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-04-01 13:50:25 -0400

    Crear instancia de base de datos
    Crea una instancia de SQLAlchemy, y genera una URI
    La URI contiene los valores necesarios para la conexion a la base de datos
    Utiliza variables de entorno para determinar la conexion a una
    base de datos MySQL, si no hay variables de entorno definidas
    Crea una conexion a una base de datos SQLite por defecto

---
+++ src/utils/db.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+
+# db = MySQL()
+class SQLAlchemyBase(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=SQLAlchemyBase)
+# Cargar variables de entorno
+def get_database_uri():
+    load_dotenv()
+    # Obtener valores de las credenciales de la base de datos MySQL
+    # Se proveen valores por defecto para asegurar que la aplicación funcione normalmente
+    username = os.getenv('DB_USERNAME', None)
+    password = os.getenv('DB_PASSWORD', None)
+    host = os.getenv('DB_HOST', None)
+    database = os.getenv('DB_DATABASE', None)
+
+    # Conectarse a una base de datos MySQL si se proveen las credenciales
+    # O conectarse a una base de datos SQLite por defecto
+    if username and password and host and database:
+        database_uri = f'mysql://{username}:{password}@{host}/{database}'
+    else:
+        database_uri = 'sqlite:///cautec.db'
+
+    return database_uri
