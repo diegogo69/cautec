de7aa6a35356152fcc890338cee92c9ae4148171
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-04-01 13:41:13 -0400

    Añadir carpeta models/
    La carpeta models/ contiene los modelos SQLAlchemy de la base de datos
    Organizandolos en archivos separados y específicos para cada modelo

---
+++ src/models/reporte.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from src.utils.db import db
+# from sqlalchemy.sql import func # access SQL functions, like default creation date and time
+
+class Reporte(db.Model):
+    __tablename__ = "reportes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(100), nullable=False)
+    tipo = db.Column(db.String(20), nullable=False)
+    estado = db.Column(db.String(20), nullable=False, default='pendiente')
+    categoria = db.Column(db.String(20))
+    descripcion = db.Column(db.Text, nullable=False)
+    diagnostico = db.Column(db.Text)
+    accion = db.Column(db.Text)
+    fecha_emision = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    # created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    fecha_atencion = db.Column(db.DateTime)
+    fecha_cierre = db.Column(db.DateTime)
+    resuelto = db.Column(db.Boolean(False), default=False)
+
+    # usuario_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Reporte('{self.titulo}', '{self.fecha_emision}')"
