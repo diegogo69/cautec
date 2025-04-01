8479a58f38bf9475665bfc6dc0e6edd2064d07a4
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-04-01 13:46:00 -0400

    Refactorizar inicializacion de base de datos

--- src/__init__.py
+++ src/__init__.py
@@ -1,19 +1,21 @@
 from flask import Flask
-from flask_mysqldb import MySQL # Usar SQL-Alchemy
+# from flask_mysqldb import MySQL # Usar SQL-Alchemy
 from src.config import Config
+from src.utils.db import db
 
-db = MySQL()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
-
-    db.init_app(app)
-
+    
     from src.routes.main import main
     from src.routes.reportes import reportes
     app.register_blueprint(main)
     app.register_blueprint(reportes)
+
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
 
     return app
 
