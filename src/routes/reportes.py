24ac7fb541281bb5a9a7c89b7d25892b5cb6be53
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-04-03 13:47:31 -0400

    Refactorizar código
    Eliminar código innecesario, simplificar expresiones y variables

--- src/routes/reportes.py
+++ src/routes/reportes.py
@@ -9,31 +9,27 @@ from src.forms.reportes import CrearReporte
 from src import db
 from sqlalchemy.sql import func
 
+
 reportes = Blueprint('reportes', __name__, url_prefix='/reportes',
                     template_folder='templates')
 
-@reportes.route('/ver-reportes', methods=['GET'])
+
+@reportes.route('/', methods=['GET'])
 @login_required
 def ver_reportes():
-    print(current_user, current_user.id)
-
     reportes = Reporte.query.all()
-    
     return render_template('reportes/ver-reportes.html', data={'reportes': reportes})
 
 # from sqlalchemy.orm import asdict
-@reportes.route('/reporte/<string:id>')
+@reportes.route('/reporte/<int:id>', methods=['GET'])
 @login_required
 def ver_reporte(id):
-    # crear_comentario.reporte_id = id
     reporte = Reporte.query.get_or_404(id)
     comentarios = Comentario.query.filter_by(reporte_id=id)#.first()
-    # reporte.fecha_atencion = func.now()
     
-    forms = {'crear_comentario': crear_comentario}
     return render_template('reportes/ver-reporte.html',
                            reporte=reporte, comentarios=comentarios,
-                           forms=forms)
+                           )
     
 
 @reportes.route('/crear-reporte', methods=['GET', 'POST'])
@@ -53,21 +49,19 @@ def crear_reporte():
         db.session.commit()
         
         flash('Tu reporte ha sido registrado exitosamente!', 'message')
-        
         return redirect(url_for('reportes.crear_reporte'))
-    
     
     elif request.method == 'GET':
         return render_template('reportes/crear-reporte.html', form=form)
 
 
-
-@reportes.route('/editar-reporte/<string:id>', methods=['GET', 'POST'])
+@reportes.route('/reporte/<int:id>/editar', methods=['GET', 'POST'])
 @login_required
 def editar_reporte(id):
     reporte = Reporte.query.get_or_404(id)
     if reporte.usuario_id != current_user.id:
         abort(403)
+
     if request.method == 'POST':
         titulo = request.form['titulo']
         descripcion = request.form['descripcion']
@@ -75,74 +69,54 @@ def editar_reporte(id):
         reporte.titulo = titulo
         reporte.descripcion = descripcion
 
-        # db.session.add(reporte)
         db.session.commit()
         
         flash(f'El reporte ID #{id} fue editado exitosamente!', 'message')
-        
         return redirect(url_for('reportes.ver_reportes'))
     
-    
     elif request.method == 'GET':
-        return render_template('reportes/editar-reporte.html', data={'reporte': reporte})
-    
-    
-    
-@reportes.route('/eliminar-reporte/<string:id>', methods=['GET', 'POST'])
+        return render_template('reportes/editar-reporte.html', reporte=reporte)
+
+
+# Ruta para eliminar reporte. Válida sólo para el método POST
+@reportes.route('/reporte/<int:id>/eliminar', methods=['POST'])
 @login_required
 def eliminar_reporte(id):
-    if request.method == 'POST':
-        reporte = Reporte.query.get_or_404(id)
-        if reporte.usuario_id != current_user.id:
-            abort(403)
+    reporte = Reporte.query.get_or_404(id)
 
-        db.session.delete(reporte)
-        db.session.commit()
-        
-        flash(f'El reporte ID #{id} se elimino exitosamente!', 'message')
-        
-        return redirect(url_for('reportes.ver_reportes'))
+    if reporte.usuario_id != current_user.id:
+        abort(403)
+
+    db.session.delete(reporte)
+    db.session.commit()
     
-    
-    elif request.method == 'GET':
-        flash('Los elementos se eliminan a través del método POST solamente', 'error')
+    flash(f'El reporte ID #{id} se elimino exitosamente!', 'message')
+    return redirect(url_for('reportes.ver_reportes'))
 
-        return redirect(url_for('reportes.ver_reportes'))
-        
 
-# @reportes.route("/crear-comentario/<int:reporte_id>", methods=["POST"])
 @reportes.route('/reporte/<int:reporte_id>/crear-comentario', methods=["POST"])
 @login_required
 def crear_comentario(reporte_id):
-    if request.method == 'POST':
-        comentario_cont = request.form['comentario']
-        # reporte_id = request.form['reporte_id']
-        
-        comentario = Comentario(comentario=comentario_cont, reporte_id=reporte_id,
-                                usuario_id=current_user.id)
+    texto_comentario = request.form['comentario']
+    comentario = Comentario(comentario=texto_comentario, reporte_id=reporte_id,
+                            usuario_id=current_user.id)
 
-        db.session.add(comentario)
-        db.session.commit()
-        flash("Tu comentario ha sido registrado exitosamente!", "message")
+    db.session.add(comentario)
+    db.session.commit()
 
-        return redirect(url_for("reportes.ver_reporte", id=reporte_id))
+    flash("Tu comentario ha sido registrado exitosamente!", "message")
     return redirect(url_for("reportes.ver_reporte", id=reporte_id))
+
 
 @reportes.route('/reporte/<int:reporte_id>/eliminar-comentario/<int:id>', methods=["POST"])
 @login_required
 def eliminar_comentario(id, reporte_id):
-    if request.method == 'POST':
-        comentario = Comentario.query.get_or_404(id)
-        if comentario.usuario_id != current_user.id:
-            abort(403)
-            
-        db.session.delete(comentario)
-        db.session.commit()
+    comentario = Comentario.query.get_or_404(id)
+    if comentario.usuario_id != current_user.id:
+        abort(403)
         
-        flash(f'El comentario ID #{id} se elimino exitosamente!', 'message')
-        
-        return redirect(url_for('reportes.ver_reporte', id=reporte_id))
+    db.session.delete(comentario)
+    db.session.commit()
     
-    elif request.method == 'GET':
-        flash('Los elementos se eliminan a través del método POST solamente', 'error')
-        return redirect(url_for('reportes.ver_reporte', id=reporte_id))
+    flash(f'El comentario ID #{id} se elimino exitosamente!', 'message')
+    return redirect(url_for('reportes.ver_reporte', id=reporte_id))
