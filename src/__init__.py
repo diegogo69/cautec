82fed3cac7770f6cd8d64b6ad05d11c5ad41c458
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-04-03 10:10:23 -0400

    Registrar rutas de autenticacion de usuarios

--- src/__init__.py
+++ src/__init__.py
@@ -1,21 +1,27 @@
 from flask import Flask
-# from flask_mysqldb import MySQL # Usar SQL-Alchemy
 from src.config import Config
 from src.utils.db import db
+from src.utils.auth import bcrypt, login_manager
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
-    
+
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+        
     from src.routes.main import main
+    from src.routes.usuarios import usuarios
     from src.routes.reportes import reportes
     app.register_blueprint(main)
+    app.register_blueprint(usuarios)
     app.register_blueprint(reportes)
 
     db.init_app(app)
     with app.app_context():
         db.create_all()
+
 
     return app
 
