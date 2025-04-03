2c57a21ca5be9783169470b0293927f5c2f8ad04
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-04-03 10:16:58 -0400

    Registrar rutas de gestion de errores
    Registrar en la instancia de las rutas de gestion de errores

--- src/__init__.py
+++ src/__init__.py
@@ -14,9 +14,11 @@ def create_app(config_class=Config):
     from src.routes.main import main
     from src.routes.usuarios import usuarios
     from src.routes.reportes import reportes
+    from src.routes.errores import errores
     app.register_blueprint(main)
     app.register_blueprint(usuarios)
     app.register_blueprint(reportes)
+    app.register_blueprint(errores)
 
     db.init_app(app)
     with app.app_context():
