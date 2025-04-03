53caba9039cc54910114262ff42394ebb15b5b6f
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-04-03 14:00:17 -0400

    Foreing key[Departamento] opcional
    Hacer que el campo departamento_id sea opcional para la clase usuarioPermitiendo que el departamento se indique al momento de crear el reporte

--- src/models/usuario.py
+++ src/models/usuario.py
@@ -4,7 +4,6 @@ from flask_login import UserMixin
 # UserMixin provides certain attributes and methods User model expects:
 # isAuthenticated, isActive, isAnonymous, get_id()
 
-
 class Usuario(db.Model, UserMixin):
     __tablename__ = "usuarios"
 
@@ -17,7 +16,7 @@ class Usuario(db.Model, UserMixin):
     nombres = db.Column(db.String(50))
     apellidos = db.Column(db.String(50))
 
-    # departamento = db.Column(db.Integer, db.ForeignKey('departamentos.id'), nullable=False)
+    departamento_id = db.Column(db.Integer, db.ForeignKey('departamentos.id')) #, nullable=False)
 
 
     def __repr__(self):
