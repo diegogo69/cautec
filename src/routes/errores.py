c9e56741d5111d21f5b2baa5283232dbd22e044e
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-04-03 13:46:23 -0400

    Añadir gestion de error 405
    Gestionar error 405 en el servidor
    Gestionar acceso a rutas a través de un método inválido
    Por ejemplo, intentar acceder a una ruta para eliminar un elemento
    desde una URL usando un método inválido get
    Evita ejecucion de las rutas desde metodos no permitidos

--- src/routes/errores.py
+++ src/routes/errores.py
@@ -3,15 +3,17 @@ from flask import Blueprint, render_template
 errores = Blueprint('errores', __name__)
 
 
-@errores.app_errorhandler(404)
-def error_404(error):
-    return render_template('errores/404.html'), 404
-
-
 @errores.app_errorhandler(403)
 def error_403(error):
     return render_template('errores/403.html'), 403
 
+@errores.app_errorhandler(404)
+def error_404(error):
+    return render_template('errores/404.html'), 404
+
+@errores.app_errorhandler(405)
+def error_405(error):
+    return render_template('errores/405.html'), 405
 
 @errores.app_errorhandler(500)
 def error_500(error):
