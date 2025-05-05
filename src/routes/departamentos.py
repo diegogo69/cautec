e774e54ad7599fab9f252ae55cd9b4bef6a8fb66
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-05-05 07:30:33 -0400

    Guardar departamentos registrados en un archivo csv
    Cada vez que se registra un nuevo departamento/area
    se crea o actualiza un archivo departamentos.csv con los departamentos registrados actualmente

--- src/routes/departamentos.py
+++ src/routes/departamentos.py
@@ -71,6 +71,7 @@ def crear_departamento():
         db.session.add(departamento)
         db.session.commit()
 
+        departamentosToCsv()
         flash("El departamento ha sido registrado exitosamente!", "success")
         return redirect(url_for("departamentos.ver_departamentos"))
 
@@ -123,3 +124,52 @@ def eliminar_departamento(id):
 
     elif request.method == "GET":
         abort(403)
+
+
+# from json import JSONEncoder, JSONDecoder
+import csv
+
+
+def departamentosToCsv():
+    departamentos_lista = []
+    departamentos_db = Departamento.query.all()
+    print()
+    print(departamentos_db)
+
+    for dep in departamentos_db:
+        id = dep.id
+        tipo = dep.tipo
+        nombre = dep.nombre
+        ubicacion = dep.ubicacion
+        nombre_coordinador = dep.nombre_coordinador
+        linea_telefonica = dep.linea_telefonica
+
+        dep_diccionario = {
+            "id": id,
+            "tipo": tipo,
+            "nombre": nombre,
+            "ubicacion": ubicacion,
+            "nombre_coordinador": nombre_coordinador,
+            "linea_telefonica": linea_telefonica,
+        }
+
+        departamentos_lista.append(dep_diccionario)
+
+    with open("local/csv/departamentos.csv", "w") as file:
+        writer = csv.DictWriter(
+            file,
+            fieldnames=[
+                "id",
+                "tipo",
+                "nombre",
+                "ubicacion",
+                "nombre_coordinador",
+                "linea_telefonica",
+            ],
+        )
+
+        writer.writeheader()
+        for dep in departamentos_lista:
+            writer.writerow(dep)
+
+    file.close()
