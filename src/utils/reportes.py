9f872adf9e5a221fe948c74dfd5ecf45f94b7673
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-05-10 10:09:14 -0400

    Crear variables para lista de dispositivos y fallas

---
+++ src/utils/reportes.py
@@ -0,0 +1,14 @@
+TIPOS_DISPOSITIVOS = [
+    "monitor",
+    "mouse",
+    "teclado",
+    "cpu",
+    "impresora",
+]
+
+FALLAS_DISPOSITIVOS = [
+    "no enciende",
+    "se apaga",
+    "se queda pegada",
+    "monitor no muestra imagen",
+]
