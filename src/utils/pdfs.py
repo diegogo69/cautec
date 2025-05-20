c28200e9216df5983c008df574d988b788458adb
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-05-20 10:48:03 -0400

    Utilizar valor config por defecto de wkhtmltopdf#

--- src/utils/pdfs.py
+++ src/utils/pdfs.py
@@ -1,13 +1,13 @@
 import pdfkit
 import jinja2
 
-def crear_pdf(data, ruta_template, ruta_css, ruta_pdf=None):
+def crear_pdf(data_template, ruta_template, ruta_css, ruta_pdf=None):
     nombre_template = ruta_template.split("/")[-1]
     carpeta_template = ruta_template.replace(nombre_template, "")
 
-    env = jinja2.Environment(loader=jinja2.FileSystemLoader(carpeta_template))
-    template = env.get_template(nombre_template)
-    html_string = template.render(data)
+    env_template = jinja2.Environment(loader=jinja2.FileSystemLoader(carpeta_template))
+    template = env_template.get_template(nombre_template)
+    html_string = template.render(data_template)
 
     # from version 0.12.6 wkhtmltopdf disables local file access by default
     # use 'enable-local-file-access' option to allow local images being rendered in the pdf  
@@ -23,10 +23,11 @@ def crear_pdf(data, ruta_template, ruta_css, ruta_pdf=None):
 
     # obtén la ruta de wkhtmltopdf con el siguiente comando:
     # linux | which wkhtmltopdf
-    # windows | where wkhtmltopdf 
-    ruta_wkhtmltopdf = '/usr/bin/wkhtmltopdf'
-    config = pdfkit.configuration(wkhtmltopdf=ruta_wkhtmltopdf)
-    # config = pdfkit.configuration() # By default pdfkit will attempt to locate this using which
+    # windows | where wkhtmltopdf
+    # location of the wkhtmltopdf binary.
+    # By default pdfkit will attempt to locate this using which
+    # ruta_wkhtmltopdf = '/usr/bin/wkhtmltopdf'
+    # config = pdfkit.configuration(wkhtmltopdf=ruta_wkhtmltopdf)
 
     # Also you can pass an opened file:
     # with open('file.html') as f:
@@ -41,7 +42,7 @@ def crear_pdf(data, ruta_template, ruta_css, ruta_pdf=None):
         output_path=ruta_pdf,
         options=options,
         css=ruta_css,
-        configuration=config,
+        # configuration=config,
         verbose=True, # Get output hints for debugging
     )
 
@@ -51,6 +52,6 @@ if __name__ == "__main__":
     template = '/home/diego/repos/cautec/src/templates/pdfs/solicitud_de_servicio.html'
     data = {}
     css = '/home/diego/repos/cautec/src/static/css/pdfs/solicitud_de_servicio.css'
-    # pdf = '/home/diego/repos/cautec/Nota_de_servico.pdf'
-    pdf = None
+    pdf = '/home/diego/repos/cautec/Nota_de_servico.pdf'
+    # pdf = None
     crear_pdf(data, template, css, pdf)
