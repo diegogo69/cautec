c5d3cbade539b2949b1b771b5ee4f6eb43341ac1
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-05-20 10:46:17 -0400

    Añadir valor por defecto de SECRET_KEY

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ database_uri = get_database_uri()
 
 # Configuración de la aplicación agrupada en una clase
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY') # Usa una clave segura
+    SECRET_KEY = os.getenv('SECRET_KEY', 'clave_por_defecto') # Usa una clave segura
     # Conección Flask-SQLAlchemy: MySQL / MariaDB o SQLite
     SQLALCHEMY_DATABASE_URI = database_uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
